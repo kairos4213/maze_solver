f93bc5bb7dbdb03d404820e32cce5e71d6a8f64a
kairos4213 <jamesdkairos@gmail.com>
2024-06-19 20:37:53 -0500

    Add Point, Line, Line.draw(), and Window.draw_line()

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from window import Window
+from window import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+
+    top_line = Line(Point(200, 200), Point(500, 200))
+    left_line = Line(Point(200, 200), Point(200, 500))
+    right_line = Line(Point(500, 200), Point(500, 500))
+    bottom_line = Line(Point(200, 500), Point(500, 500))
+
+    win.draw_line(top_line, "green")
+    win.draw_line(left_line, "green")
+    win.draw_line(right_line, "green")
+    win.draw_line(bottom_line, "green")
+
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -27,3 +27,28 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point_one, point_two):
+        self.point_one = point_one
+        self.point_two = point_two
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point_one.x,
+            self.point_one.y,
+            self.point_two.x,
+            self.point_two.y,
+            fill=fill_color,
+            width=2,
+        )
