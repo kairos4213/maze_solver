749ad1fddbdc6e9f2c786dde28e9c601f5f65da5
kairos4213 <jamesdkairos@gmail.com>
2024-06-22 12:13:16 -0500

    Add maze tests

--- cell.py
+++ cell.py
@@ -2,7 +2,7 @@ from graphics import Line, Point
 
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self._x1 = None
         self._x2 = None
         self._y1 = None
---
+++ tests.py
@@ -0,0 +1,49 @@
+import unittest
+
+from maze import Maze
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+
+    def test_maze_create_cells_wide_maze(self):
+        num_cols = 1
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+    
+    def test_maze_create_cells_tall_maze(self):
+        num_cols = 10
+        num_rows = 1
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+    
+    def test_maze_create_cells_small_maze(self):
+        num_cols = 1
+        num_rows = 1
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+    
+    def test_maze_create_cells_large_maze(self):
+        num_cols = 20
+        num_rows = 20
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+
+
+if __name__ == "__main__":
+    unittest.main()
